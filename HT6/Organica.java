bddebcdaf618d03a459d7ddbe85a60f8d36366c8
JoseFlores <flo22730@uvg.edu.gt>
2024-03-20 11:40:43 -0600

    Organica.java

--- HT6/Organica.java
+++ HT6/Organica.java
@@ -2,8 +2,9 @@ public class Organica implements Hash {
 
     @Override
     public String Codificar(String Correo) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'Codificar'");
+       
+        return Correo;
+        
     }
     
 }
